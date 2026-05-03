fd3eed3794ae44a7f3e95cdb50309dea68fb7150
JUDE <judausfer@alu.edu.gva.es>
2026-05-03 23:17:38 +0200

    Proyecto No guiado.

---
+++ Jugadores.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jugadores
+{
+    public class Jugador
+    {
+        //Campos
+        private string _nombre;
+        private int _vida;
+        private double _oro;
+        private bool _npc;
+        private int _resistencia;
+
+        // Constructor
+        public Jugador()
+        {
+
+        }
+
+        public Jugador(string nombre, int vida = 100, double oro = 0, int resistencia = 50)
+        {
+            Nombre = nombre;
+            _vida = vida;
+            _oro = oro;
+            _npc = false;
+            _resistencia = resistencia;
+        }
+
+        // Propiedades o Métodos Getters
+        public string Nombre
+        {
+            get { return _nombre; }
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException(nameof(Nombre));
+                }
+
+                _nombre = value;
+            }
+        }
+        public int Vida
+        {
+            get { return _vida; }
+        }
+        public double Oro
+        {
+            get { return _oro; }
+        }
+        public bool Npc
+        {
+            get { return _npc; }
+        }
+        public int Resistencia
+        {
+            get { return _resistencia; }
+        }
+
+        // Métodos
+        public void CambiarNombre(string nuevoNombre)
+        {
+            if (nuevoNombre == null)
+            {
+                throw new ArgumentNullException(nameof(nuevoNombre), "El nombre no puede ser null o vacío.");
+            }
+
+            _nombre = nuevoNombre;
+        }
+
+        public void AnyadirOro(int cantidad)
+        {
+            if (cantidad < 0)
+            {
+                throw new ArgumentException("La cantidad no puede ser negativa.");
+            }
+
+            _oro += cantidad;
+        }
+
+        public void QuitarOro(int cantidad)
+        {
+
+            if (cantidad < 0)
+            {
+                throw new ArgumentException("La cantidad no puede ser negativa");
+            }
+            if (cantidad > _oro)
+            {
+                throw new ArgumentOutOfRangeException("No hay suficiente oro.");
+            }
+
+            _oro -= cantidad;
+        }
+
+        public void AsignarNPC()
+        {
+            _npc = true;
+        }
+        public void DesAsignarNPC()
+        {
+            _npc = false;
+        }
+        public void AnyadirResistencia()
+        {
+            if (_npc)
+            {
+                throw new InvalidOperationException("Error de resistencia, este personaje es un npc.");
+            }
+            _resistencia += 10;
+        }
+        public void QuitarResistencia()
+        {
+            if (Resistencia <= 0)
+            {
+                throw new ArgumentOutOfRangeException("No se puede reducir la resistencia porque es 0 o menor");
+            }
+
+            _resistencia -= 10;
+        }
+        public void QuitarVida(int cantidad)
+        {
+            _vida -= cantidad;
+        }
+    }
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,161 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Jugadores;
+
+namespace TestJugador
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Verificar_AsignarNombreCorrectamente_AlCrearJugador()
+        {
+            // Preparación
+            Jugador jugador = new Jugador("Ash");
+
+            // Assert
+            Assert.AreEqual("Ash", jugador.Nombre);
+        }        
+
+        [TestMethod]
+        public void Comprobar_AnyadirOro_SeIncrementaElValor()
+        {
+            // Preparación
+            Jugador jugador = new Jugador("Ash");
+            double oroInicial = jugador.Oro;
+
+            // Acción
+            jugador.AnyadirOro(50);
+
+            // Verificar
+            Assert.AreEqual(oroInicial + 50, jugador.Oro);
+        }
+
+        [TestMethod]
+        public void Comprobar_QuitarVida_SeReduceCorrectamente()
+        {
+            // Preparación
+            Jugador jugador = new Jugador("Ash");
+            int vidaInicial = jugador.Vida;
+
+            // Acción
+            jugador.QuitarVida(20);
+
+            // Verificar
+            Assert.AreEqual(vidaInicial - 20, jugador.Vida);
+        }
+
+        [TestMethod]
+        public void Verificar_NPCEsFalse()
+        {
+            // Preparación
+            Jugador jugador = new Jugador("Ash");
+
+            // Verificar
+            Assert.IsFalse(jugador.Npc);
+        }
+
+        [TestMethod]
+        public void Comprobar_AlAsignarNPC_DevuelveTrue()
+        {
+            //Preparación
+            Jugador jugador = new Jugador("Ash");
+
+            // Acción
+            jugador.AsignarNPC();
+
+            // Verificar
+            Assert.IsTrue(jugador.Npc);
+        }
+
+        [TestMethod]
+        public void Comprobar_AlDesAsignarNPC_DevuelveFalse()
+        {
+            // Preparación
+            Jugador jugador = new Jugador("Ash");
+
+            // Acción
+            jugador.AsignarNPC();
+            jugador.DesAsignarNPC();
+
+            // Verificar
+            Assert.IsFalse(jugador.Npc);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void QuitarOro_MasDelDisponible_LanzaExcepcion()
+        {
+            // Preparación
+            Jugador jugador = new Jugador("Ash");
+            jugador.AnyadirOro(50);
+
+            // Act
+            jugador.QuitarOro(100);
+
+            // la afirmación es manejado por el atributo ExpectedException
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void QuitarResistencia_CuandoNoEsPosible_LanzaExcepcion()
+        {
+            // Preparación 
+            // La resistencia inicializa a 50 por defecto.
+            Jugador jugador = new Jugador("Ash");
+
+            // Quitar varias veces hasta llegar a 0
+            jugador.QuitarResistencia(); // 40
+            jugador.QuitarResistencia(); // 30
+            jugador.QuitarResistencia(); // 20
+            jugador.QuitarResistencia(); // 10
+            jugador.QuitarResistencia(); // 0
+
+            // Una más para lanzar Excepción
+            jugador.QuitarResistencia();
+
+            // la afirmación es manejado por el atributo ExpectedException
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void CambiarNombreANull_LanzaExcepcion()
+        {
+            // Preparación
+            var jugador = new Jugador("Ash");
+
+            // Acción
+            jugador.CambiarNombre(null);
+
+            // la afirmación es manejado por el atributo ExpectedException
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void CrearJugador_NombreNull_LanzaExcepcion()
+        {
+            // Acción
+            var jugador = new Jugador(null);
+
+            // la afirmación es manejado por el atributo ExpectedException
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void AnyadirResistencia_ConNPC_LanzaExcepcion()
+        {
+            // Preparación
+            Jugador jugador = new Jugador("Ash");
+            jugador.AsignarNPC();
+
+
+            // Acción
+            jugador.AnyadirResistencia();
+
+            // la afirmación es manejado por el atributo ExpectedException
+        }
+
+    }
+}
